63d6000b68afb550a7479015c97f6dcdfc78c74c
Charles Labourier <charles@sherpa-advisory.com>
2020-05-13 20:58:00 +0200

    Fix typo in evaluation message

--- internal/slack/defaults.go
+++ internal/slack/defaults.go
@@ -4,12 +4,12 @@ import "github.com/gustavobelfort/42-jitsi/internal/config"
 
 func defaultPostMessageParameters() *PostMessageParameters {
 	return &PostMessageParameters{
-		Text:      "This is the link for you corretion that will take place in 15 minutes",
+		Text:      "This is the link for your evaluation that will take place in 15 minutes.",
 		Username:  config.Conf.SlackThat.Username,
 		Workspace: config.Conf.SlackThat.Workspace,
 		Attachments: []Attachment{
 			{
-				Title:     "42 Correction",
+				Title:     "42 Evaluation",
 				Pretext:   "Make sure to arrive on time and follow the remote correction guidelines !",
 				TitleLink: "https://meet.jit.si/correction",
 				Color:     "#36a64f",
